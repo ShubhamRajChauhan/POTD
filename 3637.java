60e30bcb729bbb40f5b30c7ffb33165840e4e870
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-03 22:31:11 +0530

    leetcode 3637

---
+++ 3637.java
@@ -0,0 +1,34 @@
+/* 
+Leetcode : 3637
+https://leetcode.com/problems/trionic-array-i/description/?envType=daily-question&envId=2026-02-03
+*/
+
+
+
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if(n < 4) return false;
+
+        int i = 0;
+        while(i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        if(i == 0) {
+            return false;
+        } 
+
+        int p = i;
+        while(i + 1 < n && nums[i] > nums[i + 1])
+            i++;
+        if(i == p) {
+            return false;
+        } 
+
+        int q = i;
+        while(i + 1 < n && nums[i] < nums[i + 1])
+            i++;
+
+        return i == n - 1 && q > p  && q < n - 1;
+    }
+}
