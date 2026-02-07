db57f4df84dc194e04dd402623ea3b11ac33bad7
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-07 18:22:39 +0530

    leetcode 3634

---
+++ 3634.java
@@ -0,0 +1,30 @@
+/* 
+Leetcode : 3634
+https://leetcode.com/problems/minimum-removals-to-balance-array/description/?source=submission-noac
+*/
+
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int length = 1;
+        int i = 0;
+        int j = 0;
+
+        while(j < n) {
+            int minEl = nums[i];
+            int maxEl = nums[j];
+
+            while(i < j && maxEl  > (long)k * minEl) {
+                i++;
+                minEl = nums[i];
+            }
+
+            length = Math.max(length, j-i+1);
+            j++;
+        }
+
+        return n - length;
+    }
+}
