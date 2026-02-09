6f46a20b89a466f978c575ef936ff6fd1efa50bd
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-09 10:33:53 +0530

    leetcode 1382

---
+++ 1382.java
@@ -0,0 +1,29 @@
+/* 
+Leetcode : 1382
+https://leetcode.com/problems/balance-a-binary-search-tree/description/?envType=daily-question&envId=2026-02-09
+*/
+
+
+class Solution {
+    private void inOrder(TreeNode root, ArrayList<Integer> list) {
+        if(root == null) return;
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+
+    private TreeNode makeBalance(int left, int right, ArrayList<Integer> list) {
+        if(left > right) return null;
+
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = makeBalance(left, mid - 1, list);
+        root.right = makeBalance(mid + 1, right, list);
+        return root;
+    }
+    public TreeNode balanceBST(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        return makeBalance(0, list.size() - 1, list);
+    }
+}
