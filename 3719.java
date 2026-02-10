24c6be05c262adb40711291a2e944d36e1dadc37
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-10 18:36:08 +0530

    leetcode 3719

---
+++ 3719.java
@@ -0,0 +1,22 @@
+/* 
+Leetcode : 3719
+https://leetcode.com/problems/longest-balanced-subarray-i/?envType=daily-question&envId=2026-02-10
+*/
+
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        int maxLength = 0;
+        for(int i=0; i<n; i++) {
+            Set<Integer> even = new HashSet<>();
+            Set<Integer> odd = new HashSet<>();
+            for(int j=i; j<n; j++) {
+                if(nums[j] % 2 == 0) even.add(nums[j]);
+                else odd.add(nums[j]);
+
+                if(even.size() == odd.size()) maxLength = Math.max(maxLength, j-i+1);
+            }
+        }
+        return maxLength;
+    }
+}
