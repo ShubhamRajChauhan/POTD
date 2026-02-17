2a64764ca11b081e610a02859d243e59e942a944
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-17 09:50:33 +0530

    401. Binary Watch

---
+++ 401.java
@@ -0,0 +1,19 @@
+/* 
+Leetcode : 401
+https://leetcode.com/problems/binary-watch/description/
+*/
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+        for(int hour = 0; hour < 12; hour++) {
+            for(int minute = 0; minute < 60; minute++) {
+                int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+                if(totalBits == turnedOn) {
+                    result.add(hour + ":" + String.format("%02d", minute));
+                }
+            }
+        }
+        return result;
+    }
+}
