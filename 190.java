248d3ad4532b6db576e9be6a270231140455686e
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-17 09:48:51 +0530

    190. Reverse Bits

---
+++ 190.java
@@ -0,0 +1,17 @@
+/* 
+Leetcode : 190
+https://leetcode.com/problems/reverse-bits/
+*/
+
+
+public class 190 {
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i=0; i<32; i++){
+            result <<= 1;
+            result = (result | (n & 1));
+            n >>= 1;
+        }  
+        return result;
+    }
+}
