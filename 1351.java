0557cefe8418364956905bda29c5370069e3a480
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-17 09:35:15 +0530

    1351. Count Negative Numbers in a Sorted Matrix

--- 1351.java
+++ 1351.java
@@ -1,7 +1,6 @@
 /* 
 Leetcode : 1351
-https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/?envType=daily-question&envId=2025-12-28
-
+https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
 
 */
 
