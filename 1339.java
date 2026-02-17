6331ee11ff31af29f1a4999b72a8361d3895b9bd
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-17 09:34:35 +0530

    1339. Maximum Product of Splitted Binary Tree

--- 1339.java
+++ 1339.java
@@ -1,6 +1,6 @@
 /* 
 Leetcode : 1339
-https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/description/
 */
 
 
