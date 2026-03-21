63d24e245a280224927b8bc116db9bd2dcabca72
Shubham <shubhamrajchauhan07@gmail.com>
2026-03-21 06:15:07 +0530

    3643. Flip Square Submatrix Vertically

---
+++ 3643.java
@@ -0,0 +1,27 @@
+/* 
+Leetcode : 3643
+https://leetcode.com/problems/flip-square-submatrix-vertically/description/
+*/
+
+
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        
+        int top = x;
+        int bottom = x + k - 1;
+        
+        while(top < bottom) {
+            
+            for(int col = y; col < y + k; col++) {
+                int temp = grid[top][col];
+                grid[top][col] = grid[bottom][col];
+                grid[bottom][col] = temp;
+            }
+            
+            top++;
+            bottom--;
+        }
+        
+        return grid;
+    }
+}
