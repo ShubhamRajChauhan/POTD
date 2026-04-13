482675850e26c7a82c2fea4f49b1fed4537654ee
Shubham <shubhamrajchauhan07@gmail.com>
2026-04-13 18:04:21 +0530

    1848. Minimum Distance to the Target Element

---
+++ 1848.java
@@ -0,0 +1,20 @@
+/* 
+Leetcode : 1848
+https://leetcode.com/problems/minimum-distance-to-the-target-element/description/
+*/
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int n = nums.length;
+
+        int result = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n && result > Math.abs(i - start); i++) {
+            if(nums[i] == target) {
+                result = Math.min(result, Math.abs(i - start));
+            }
+        }
+
+        return result;
+    }
+}
