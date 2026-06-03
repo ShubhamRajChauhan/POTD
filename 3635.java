92fef79e21a889938f457652cb717299503bceca
Shubham <shubhamrajchauhan07@gmail.com>
2026-06-03 22:17:55 +0530

    3635. Earliest Finish Time for Land and Water Rides II

---
+++ 3635.java
@@ -0,0 +1,28 @@
+/* 
+Leetcode : 3635
+https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-ii/description/
+*/
+
+
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int landFinish = findFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
+        int waterFinish = findFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
+
+        return Math.min(landFinish, waterFinish);
+    }
+
+    private int findFinishTime(int[] start1, int[] duration1, int[] start2, int[] duration2) {
+        int finish1 = Integer.MAX_VALUE;
+        for(int i=0; i<start1.length; i++) {
+            finish1 = Math.min(finish1, start1[i] + duration1[i]);
+        }
+
+        int finish2= Integer.MAX_VALUE;
+        for(int i=0; i<start2.length; i++) {
+            finish2 = Math.min(finish2, Math.max(finish1, start2[i]) + duration2[i]);
+        }
+
+        return finish2;
+    }
+}
