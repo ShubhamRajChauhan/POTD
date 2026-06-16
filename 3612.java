1433fdebd1ad2efca59e0d8643db37a3d42cade8
Shubham <shubhamrajchauhan07@gmail.com>
2026-06-16 22:31:41 +0530

    3612. Process String with Special Operations I

---
+++ 3612.java
@@ -0,0 +1,23 @@
+/* 
+Leetcode : 3612
+https://leetcode.com/problems/process-string-with-special-operations-i/description/
+*/
+
+
+class Solution {
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+        for(char ch : s.toCharArray()) {
+            if(ch == '*') {
+                if(result.length() > 0) result.deleteCharAt(result.length() - 1);
+            } else if (ch == '#') {
+                result.append(result.toString());
+            } else if (ch == '%') {
+                result.reverse();
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
