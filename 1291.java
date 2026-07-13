943d8e9328c322390b7885311bdd08dd8ac64fc7
Shubham <shubhamrajchauhan07@gmail.com>
2026-07-14 01:04:13 +0530

    1291. Sequential Digits

---
+++ 1291.java
@@ -0,0 +1,30 @@
+/* 
+Leetcode : 1291
+https://leetcode.com/problems/sequential-digits/description/
+*/
+
+
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=8; i++) {
+            queue.offer(i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int temp = queue.poll();
+
+            if(temp >= low && temp <= high) {
+                result.add(temp);
+            }
+
+            int lastDigit = temp % 10;
+            if(lastDigit + 1 <= 9) {
+                queue.offer(temp * 10 + (lastDigit + 1));
+            }
+        }
+
+        return result;
+    }
+}
